f17826f050fb06e794881b98380488e61a0ad7cb
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-09-30 13:14:17 +0200

    function return types & void

--- app.ts
+++ app.ts
@@ -1,30 +1,13 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
+function printResult(num: number): void {
+  console.log("result: " + num);
+  return;
+}
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+console.log(printResult(add(12, 5)));
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+
+let someValue: undefined;
