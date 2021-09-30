8f33861a7d9a65e6c9d6c0c0985d529be0ac98ff
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-09-30 12:00:22 +0200

    using types

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log('Your code goes here...');
+function add(n1: number, n2:number){
+  return n1 + n2;
+}
+
+const number1 = '5';
+const number2 = 2.4;
+
+console.log(add(number1, number2));
