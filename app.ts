150f333898d19f4be5871ff83eab69a44fded457
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-09-30 12:52:40 +0200

    working with enums

--- app.ts
+++ app.ts
@@ -1,19 +1,18 @@
 // const person: object = { -> throws error when person.name
 // const person: {} = {  -> the same as above
 
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+enum Role {
+  ADMIN = 4, READ_ONLY = 100, AUTHOR = "DUPA"
+}
+
+const person = {
   age: 30,
   name: "Maximillian",
   hobbies: ["sports", "cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN
 };
 
-person.role.push("admin"); // this will work EXCEPTION!!
+// person.role.push("admin"); // this will work EXCEPTION!!
 // person.role[1] = 20; -> will throw an eror
 
 // let favouriteActivities: string[] -> will throw an error
@@ -28,4 +27,16 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+console.log(Role)
+
+const CustomRole = {};
+CustomRole[0] = "ADMIN";
+CustomRole["ADMIN"] = 0;
+console.log(CustomRole);
+
+
+if(person.role === Role.AUTHOR){
+  console.log("this will work")
 }
