03d87e52bf8d3a7505282877c258e284256a4b08
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-09-30 12:25:16 +0200

    array types

--- app.ts
+++ app.ts
@@ -1,12 +1,22 @@
 // const person: object = { -> throws error when person.name
 // const person: {} = {  -> the same as above
 
-const person: {
-  age: number;
-  name: string;
-} = {
+const person = {
   age: 30,
   name: "Maximillian",
+  hobbies: ["sports", "cooking"],
 };
 
+// let favouriteActivities: string[] -> will throw an error
+
+// let favouriteActivities: any[]
+// favouriteActivities = ['sports', 1]
+
+let favouriteActivities: string[];
+favouriteActivities = ["sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
