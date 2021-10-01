9b3460689c28fee376db05b339dcd2d1a92f8cf3
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-01 14:37:00 +0200

    next-gen js examples (>es6)

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,60 @@
-let appId = 'abc';
-const button = document.querySelector('button')!;
+// const userName = 'Max';
+// // userName = 'Maximilian';
+// let age = 30;
 
-function add(n1: number, n2: number) {
-  if (n1 + n2 > 0) {
-    return n1 + n2;
-  }
-  return;
-}
+// age = 29;
 
-function clickHandler(message: string) {
-  // let userName = 'Max';
-  console.log('Clicked! ' + message);
-}
-// a comment
-if (button) {
-  button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+
+// console.log(isOld);
+
+// console.log(result);
+
+// const add = (a: number, b: number = 1) => a + b;
+
+// const printOutput: (a: number | string) => void = output => console.log(output);
+
+// const button = document.querySelector('button');
+
+// if (button) {
+//   button.addEventListener('click', event => console.log(event));
+// }
+
+// printOutput(add(5));
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: 'Max',
+  age: 30
+};
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
