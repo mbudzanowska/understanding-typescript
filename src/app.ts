f19109315f57891a7601754da01ca11a45392321
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-04 09:22:47 +0200

    class example

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,19 @@
-// const userName = 'Max';
-// // userName = 'Maximilian';
-// let age = 30;
+class Department {
+  name: string;
 
-// age = 29;
+  constructor(n: string) {
+    this.name = n;
+  }
 
-// function add(a: number, b: number) {
-//   let result;
-//   result = a + b;
-//   return result;
-// }
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
+}
 
-// if (age > 20) {
-//   let isOld = true;
-// }
+const accounting = new Department('Accounting');
 
-// console.log(isOld);
+accounting.describe();
 
-// console.log(result);
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
-// const add = (a: number, b: number = 1) => a + b;
-
-// const printOutput: (a: number | string) => void = output => console.log(output);
-
-// const button = document.querySelector('button');
-
-// if (button) {
-//   button.addEventListener('click', event => console.log(event));
-// }
-
-// printOutput(add(5));
-
-const hobbies = ['Sports', 'Cooking'];
-const activeHobbies = ['Hiking'];
-
-activeHobbies.push(...hobbies);
-
-const person = {
-  firstName: 'Max',
-  age: 30
-};
-
-const copiedPerson = { ...person };
-
-const add = (...numbers: number[]) => {
-  return numbers.reduce((curResult, curValue) => {
-    return curResult + curValue;
-  }, 0);
-};
-
-const addedNumbers = add(5, 10, 2, 3.7);
-console.log(addedNumbers);
-
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-
-console.log(hobbies, hobby1, hobby2);
-
-const { firstName: userName, age } = person;
-
-console.log(userName, age, person);
+accountingCopy.describe();
