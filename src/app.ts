b5dfd7b6a8712350797085463990059c5b5de51a
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-04 18:08:02 +0200

    generic utility types

--- src/app.ts
+++ src/app.ts
@@ -90,3 +90,25 @@ objStorage.addItem({ name: "Max" });
 
 objStorage.removeItem(obj);
 console.log(objStorage.getItems());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal;
+}
+
+const names: Readonly<string[]> = ["Max", "Anna"];
+names.push("Manu");
+names.pop();
