832e0b66afb39372fdf00ccab8e71a3997b4e5f5
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-04 17:34:10 +0200

    generic function

--- src/app.ts
+++ src/app.ts
@@ -26,5 +26,22 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 console.log(merge({ name: "Max" }, { age: 30 }));
 
 // const mergedObj = merge({ name: "Max" }, 30); // constrained with object type (extends), will not work
-const mergedObj = merge({ name: "Max" }, {age: 30});
+const mergedObj = merge({ name: "Max" }, { age: 30 });
 mergedObj.name; // this will work!
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptionText = "got no value";
+  if (element.length > 0) {
+    descriptionText = "Got " + element.length;
+  }
+  return [element, descriptionText];
+}
+
+
+console.log(countAndDescribe('Hi there!'))
+console.log(countAndDescribe(['Hi there!', "whatever"]))
+console.log(countAndDescribe([]))
