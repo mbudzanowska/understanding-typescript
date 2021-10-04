a8032181c8073ef4ebb2fb2421bee1b7a7f55772
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-04 14:39:16 +0200

    index properties

--- src/app.ts
+++ src/app.ts
@@ -130,4 +130,11 @@ if(userInputElement){
 
 userInputElement.value = "Hi there"
 
+interface ErrorContainer { 
+  [prop: string] : string;
+}
 
+const errorBag: ErrorContainer = {
+  email: 'Not a valid email',
+  username: "Must start with a capital letter"
+}
