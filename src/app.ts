dc665876777de1183e17291db666bf5d0746b9f1
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-04 14:46:10 +0200

    function overloads

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,10 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: number, b: string): string;
+function add(a: string, b: number): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     // this is a type guard!
@@ -40,6 +44,11 @@ function add(a: Combinable, b: Combinable) {
   }
   return a + b;
 }
+
+const result = add("Max", " Schwarz");
+const result2 = add(3, 3);
+const result3 = add("Max", 3);
+result.split(" ");
 
 type UnknownEmployee = Employee | Admin;
 
@@ -79,7 +88,7 @@ function useVehicle(vehicle: Vehicle) {
   // if ("loadCargo" in vehicle) {
   //   vehicle.loadCargo(1000);
   // }
-  if(vehicle instanceof Truck) {
+  if (vehicle instanceof Truck) {
     vehicle.loadCargo(1000);
   }
 }
@@ -88,53 +97,57 @@ useVehicle(v1);
 useVehicle(v2);
 
 interface Bird {
-  type: 'bird';
+  type: "bird";
   flyingSpeed: number;
 }
 
 interface Horse {
-  type: 'horse';
+  type: "horse";
   runningSpeed: number;
 }
 
 type Animal = Bird | Horse;
 
-function moveAnimal(animal:Animal) {
+function moveAnimal(animal: Animal) {
   let speed;
-  switch(animal.type){
-    case 'bird': {
-      speed = animal.flyingSpeed
+  switch (animal.type) {
+    case "bird": {
+      speed = animal.flyingSpeed;
       break;
     }
-    case 'horse': {
+    case "horse": {
       speed = animal.runningSpeed;
       break;
     }
   }
-    console.log("Moving with speed: " + speed)
+  console.log("Moving with speed: " + speed);
 }
 
-moveAnimal({type: 'bird', flyingSpeed: 500});
+moveAnimal({ type: "bird", flyingSpeed: 500 });
 
-const paragraph = document.querySelector('p');
+const paragraph = document.querySelector("p");
 
-const paragraph2 = document.getElementById('message-output');
+const paragraph2 = document.getElementById("message-output");
 
-const userInputElement = <HTMLInputElement>document.getElementById('user-input')!; // NOT FOR REACT!!!
-const userInputElement2 = document.getElementById('user-input')! as HTMLInputElement; // FOR REACT!!!
+const userInputElement = <HTMLInputElement>(
+  document.getElementById("user-input")!
+); // NOT FOR REACT!!!
+const userInputElement2 = document.getElementById(
+  "user-input"
+)! as HTMLInputElement; // FOR REACT!!!
 
 // if not ! then you need to use if statement to check if html element truly exists
-if(userInputElement){
-  (userInputElement as HTMLInputElement).value = "Hi there!"
+if (userInputElement) {
+  (userInputElement as HTMLInputElement).value = "Hi there!";
 }
 
-userInputElement.value = "Hi there"
+userInputElement.value = "Hi there";
 
-interface ErrorContainer { 
-  [prop: string] : string;
+interface ErrorContainer {
+  [prop: string]: string;
 }
 
 const errorBag: ErrorContainer = {
-  email: 'Not a valid email',
-  username: "Must start with a capital letter"
-}
+  email: "Not a valid email",
+  username: "Must start with a capital letter",
+};
