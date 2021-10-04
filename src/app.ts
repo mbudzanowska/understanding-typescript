ab63f22bdb30bf280b4ef04dd87f27195f1271c0
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-04 17:46:29 +0200

    generic classes

--- src/app.ts
+++ src/app.ts
@@ -53,3 +53,40 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: "Max" }, "name"));
+
+class DataStorage<T> {
+  //class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manuel");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Manu" }); this will not work because each object is a new object
+// objStorage.addItem({ name: "Max" });
+
+// objStorage.removeItem({ name: "Manu" });
+const obj = { name: "Manu" };
+objStorage.addItem(obj);
+objStorage.addItem({ name: "Max" });
+
+objStorage.removeItem(obj);
+console.log(objStorage.getItems());
