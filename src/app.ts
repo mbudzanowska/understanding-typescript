1922a97f3099efb1473ae6af7339b7f1ec89e812
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-04 12:08:14 +0200

    getters and setters

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -27,18 +27,44 @@ class Department {
 class ITDepartment extends Department {
   admins: string[];
   constructor(id: string, admins: string[]) {
-    super(id, 'IT');
+    super(id, "IT");
     this.admins = admins;
   }
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    } else {
+      throw new Error("No report found");
+    }
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please add a valid report");
+    } else {
+      this.addReport(value);
+    }
+  }
+
   constructor(id: string, private reports: string[]) {
-    super(id, 'Accounting');
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string) {
+    if (name === "Max") {
+      this.employees.push(name);
+    }
   }
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
@@ -46,24 +72,29 @@ class AccountingDepartment extends Department {
   }
 }
 
-const it = new ITDepartment('d1', ['Max']);
+const it = new ITDepartment("d1", ["Max"]);
 
-it.addEmployee('Max');
-it.addEmployee('Manu');
+it.addEmployee("Max");
+it.addEmployee("Manu");
 
 // it.employees[2] = 'Anna';
 
 it.describe();
-it.name = 'NEW NAME';
+it.name = "NEW NAME";
 it.printEmployeeInformation();
 
 console.log(it);
 
-const accounting = new AccountingDepartment('d2', []);
-
-accounting.addReport('Something went wrong...');
-
+const accounting = new AccountingDepartment("d2", []);
+accounting.mostRecentReport = "Dupa";
+accounting.addReport("Something went wrong...");
+accounting.mostRecentReport;
 accounting.printReports();
+
+accounting.addEmployee("Max");
+accounting.addEmployee("not Max");
+
+console.log(accounting);
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
