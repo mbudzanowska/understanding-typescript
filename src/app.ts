35c6869b7e19ec6921f065a6285acaaa82fdf037
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-05 11:52:05 +0200

    more useful decorator example

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,22 @@ function Logger(logString: string) {
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
-  }
+  };
 }
 
-@Logger('LOGGING - PERSON')
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+//@Logger('LOGGING - PERSON')
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
